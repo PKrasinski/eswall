2f91a10d887f5a1e71beceb65ab918d76475be50
Przemysław Krasiński <przemek.krasinski@gmail.com>
2022-07-17 14:50:47 -0500

    feat(core): Apply observer pattern to Wall class

--- packages/core/src/core/Wall.ts
+++ packages/core/src/core/Wall.ts
@@ -1,18 +1,31 @@
 import { WallElement, WallElementProperties } from "../elements"
+import { Publisher, PublisherEvent } from "../shared/ObserverPattern"
 import { WallElementId } from "../value_objects"
 
-export class Wall {
+export class ElementEvent extends PublisherEvent {
+    constructor (
+        public element: WallElement
+    ) { super() }
+}
+
+export class NewElementEvent extends ElementEvent {}
+export class RemoveElementEvent extends ElementEvent {}
+export class ChangeElementPropertiesEvent extends ElementEvent {}
+
+export class Wall extends Publisher {
     constructor(
         private elements: WallElement[]
-    ) { }
+    ) { super() }
 
     addElement(element: WallElement) {
         this.elements.push(element)
+        this.notify(new NewElementEvent(element))
     }
 
     removeElement(id: WallElementId) : WallElement {
         const index = this.elements.findIndex(element => !element.idEqual(id))
         const element = this.elements.splice(index, 1)[0]
+        this.notify(new RemoveElementEvent(element))
         return element
     }
 
@@ -24,6 +37,7 @@ export class Wall {
         const element = this.findElementById(id)
         if (!element) throw new Error('Element not founded')
         const oldProperties = element.changeProperties(properties)
+        this.notify(new ChangeElementPropertiesEvent(element))
         return oldProperties
     }
 }
